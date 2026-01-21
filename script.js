86a55782e782ade94e1dc9a9f64318fc2fff49ba
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2026-01-21 17:29:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	s= s.trim()
+	if (s.length === 0) return ''
+
+	let result = ''
+	for (let i = 0; i < s; i++) {
+		result+= s[i]
+		if (s[i] === " " ) {
+			return result
+		}
+	}
+	return ' '
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+?const s = prompt("Enter String:");
 alert(firstWord(s));
